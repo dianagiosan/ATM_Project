67d4900e7a14313abf3cef74a3336b6d6405db15
diana <dianagiosan@yahoo.com>
2020-07-14 13:33:17 +0300

    Implemented splitIntoBills method.

--- src/ATM.java
+++ src/ATM.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class ATM {
@@ -23,9 +25,26 @@ public class ATM {
 	}
 	
 	public ATMOutput splitIntoBills(int cashToWithdraw) {
+		availableBills.sort((billEntry1, billEntry2) -> billEntry2.getBillValue() - billEntry1.getBillValue());
+		int currentBillIndex = 0;
+		int currentBillCounter;
 		List<billEntry> returnBills = new ArrayList<>();
-		returnBills.add(new billEntry(0, 0));
-		return new ATMOutput(returnBills, "Not okay");
+		while (cashToWithdraw > 0) {
+			currentBillCounter = 0;
+			while (availableBills.get(currentBillIndex).getBillAmount() > 0 &&
+			cashToWithdraw >= availableBills.get(currentBillIndex).getBillValue()){
+				cashToWithdraw -= availableBills.get(currentBillIndex).getBillValue();
+				availableBills.get(currentBillIndex).decreaseAmount();
+				currentBillCounter++;
+				
+			}
+			if(currentBillCounter > 0)
+			returnBills.add(new billEntry(availableBills.get(currentBillIndex).getBillValue(), currentBillCounter));
+			currentBillIndex++;
+			
+		}
+		String message = "OK";
+		return new ATMOutput(returnBills, message);
 	}
 	
 	
