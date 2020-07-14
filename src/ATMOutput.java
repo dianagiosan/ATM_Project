2a6737a79495255f5b9f6bf376b0469c9708b93a
diana <dianagiosan@yahoo.com>
2020-07-14 12:50:20 +0300

    Added the ATMOutput class, as it is meant to be the return type of the splitIntoBills method. The method returns the list of bills and a message (ok or error).

---
+++ src/ATMOutput.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class ATMOutput {
+	List<billEntry> bills;
+	String message;
+	public ATMOutput(List<billEntry> bills, String message){
+		this.bills = bills;
+		this.message = message;
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		ATMOutput atmOutput = (ATMOutput) o;
+		return bills.equals(atmOutput.bills) &&
+			message.equals(atmOutput.message);
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(bills, message);
+	}
+	public String toString() {
+		return message + " Here is your money: " + bills;
+	}
+}
