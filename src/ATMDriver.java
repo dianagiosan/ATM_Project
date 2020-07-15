17040fcaf0e02fd3f0e6c406308be394c06c838b
diana <dianagiosan@yahoo.com>
2020-07-15 10:54:30 +0300

    Fixed bug.

--- src/ATMDriver.java
+++ src/ATMDriver.java
@@ -2,11 +2,12 @@ import java.util.Scanner;
 
 public class ATMDriver {
 	public static void main(String[] args) {
-		while(true) {
-		Scanner scanner = new Scanner(System.in);
-		int cashToWithdraw = scanner.nextInt();
 		ATM thisATM = new ATM();
-		System.out.println(thisATM.splitIntoBills(cashToWithdraw));
-	}
+		while (true) {
+			Scanner scanner = new Scanner(System.in);
+			int cashToWithdraw = scanner.nextInt();
+			System.out.println(thisATM.splitIntoBills(cashToWithdraw));
+		}
+		
 	}
 }
