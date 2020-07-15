c4b64fd13040691720ade2df40cae1fd32259766
diana <dianagiosan@yahoo.com>
2020-07-15 13:58:46 +0300

    Fixed bug. Created a deep copy of the bill configuration list in the ATM to work with it when trying to extract the cash. If the transaction was not successful, we do NOT update the configuration as no bills where removed from the ATM.

--- src/ATM.java
+++ src/ATM.java
@@ -3,6 +3,9 @@ import java.util.List;
 
 public class ATM { //main class that models the ATM functionality
 	
+	boolean critical100MessageSent = false; //in order to avoid sending the warning mail twice for the same event
+	boolean warning100MessageSent = false;
+	boolean warning50MessageSent = false;
 	private List<billEntry> availableBills = new ArrayList<>(); //the list of available bills in the ATM
 	
 	public ATM() { //initialize the available bills every time an ATM object is created
@@ -19,6 +22,7 @@ public class ATM { //main class that models the ATM functionality
 		availableBills.addAll(newConfiguration);
 		
 	}
+	
 	public List<billEntry> getAvailableBills() {
 		return availableBills;
 	}
@@ -30,46 +34,36 @@ public class ATM { //main class that models the ATM functionality
 		int currentBillIndex = 0; //the current bill in the list that we try to include in the output
 		int currentBillCounter;//how many bills like the current one were we able to include so far?
 		List<billEntry> returnBills = new ArrayList<>();
-		boolean critical100MessageSent = false; //in order to avoid sending the warning mail twice for the same event
-		boolean warning100MessageSent = false;
-		boolean warning50MessageSent = false;
 		String message;
 		boolean OKtoContinue = false;
 		for (billEntry entry : availableBills) {
-			if (entry.getBillAmount() != 0) {
+			if (entry.getBillAmount() != 0) { //first, check to see if the ATM is not empty
 				OKtoContinue = true;
 				break;
 			}
 		}
 		if (!OKtoContinue)
 			try {
-				throw new NotEnoughCashLeftException(); //throw exception
+				throw new NotEnoughCashLeftException(); //throw exception, there's no more cash in the ATM
 			} catch (NotEnoughCashLeftException e) {
 				e.printStackTrace();
 				message = "Transaction denied";
 				returnBills.add(new billEntry(0, 0)); //no money outputted
 				return new ATMOutput(returnBills, message);
 			}
+		List<billEntry> availableBillsCopy = new ArrayList<>(); //create a working copy of the ATM bills
+		for (billEntry entry : availableBills) {
+			billEntry entryCopy = new billEntry(entry.getBillValue(), entry.getBillAmount());
+			availableBillsCopy.add(entryCopy);
+		}
 		
-		while (cashToWithdraw > 0 && currentBillIndex < availableBills.size()) { //we still have to withdraw cash
+		while (cashToWithdraw > 0 && currentBillIndex < availableBillsCopy.size()) { //we still have to withdraw cash
 			currentBillCounter = 0;
 			
-			while (availableBills.get(currentBillIndex).getBillAmount() > 0 &&
-				cashToWithdraw >= availableBills.get(currentBillIndex).getBillValue()) { //try to include one more of the current bill
-				cashToWithdraw -= availableBills.get(currentBillIndex).getBillValue();//decrease the goal withdrawal sum
-				availableBills.get(currentBillIndex).decreaseAmount(); //lower the amount of the current bill in the list
-				if (availableBills.get(0).getBillAmount() < 5 && !critical100MessageSent) {
-					MockMail.send("Number of 100 bills is CRITICAL.");
-					critical100MessageSent = true;
-				} else if (availableBills.get(0).getBillAmount() < 10 && !warning100MessageSent) {
-					MockMail.send("Warning. Number of 100 bills under 20%.");
-					warning100MessageSent = true;
-				}
-				if (availableBills.get(1).getBillAmount() <= 7 && !warning50MessageSent) {
-					
-					MockMail.send("Warning. Number of 50 bills is under 15%.");
-					warning50MessageSent = true;
-				} //checked to see if it is the case to send the warning mail
+			while (availableBillsCopy.get(currentBillIndex).getBillAmount() > 0 &&
+				cashToWithdraw >= availableBillsCopy.get(currentBillIndex).getBillValue()) { //try to include one more of the current bill
+				cashToWithdraw -= availableBillsCopy.get(currentBillIndex).getBillValue();//decrease the goal withdrawal sum
+				availableBillsCopy.get(currentBillIndex).decreaseAmount(); //lower the amount of the current bill in the list
 				currentBillCounter++;//we've added one more of the current bill
 			}
 			
@@ -79,7 +73,8 @@ public class ATM { //main class that models the ATM functionality
 			
 		}
 		
-		if (cashToWithdraw > 0) { //if the cash could not be withdrawn, it's because there's not enough money left
+		
+		if (cashToWithdraw > 0) { //if the cash could not be withdrawn, it's because the bill configuration in the ATM doesn't allow it
 			message = "Transaction denied";
 			returnBills.clear();
 			returnBills.add(new billEntry(0, 0)); //no money outputted
@@ -88,7 +83,24 @@ public class ATM { //main class that models the ATM functionality
 			} catch (TransactionNotPossibleException e) {
 				e.printStackTrace();
 			}
-		} else message = "Transaction approved";
+		} else {
+			message = "Transaction approved"; //if the transaction was successful, update the new bill configuration in the ATM
+			availableBills.clear();
+			availableBills.addAll(availableBillsCopy);
+			//check to see if we have to send a warning mail
+			if (availableBills.get(0).getBillAmount() < 5 && !critical100MessageSent) {
+				MockMail.send("Number of 100 bills is CRITICAL.");
+				critical100MessageSent = true;
+			} else if (availableBills.get(0).getBillAmount() < 10 && !warning100MessageSent) {
+				MockMail.send("Warning. Number of 100 bills under 20%.");
+				warning100MessageSent = true;
+			}
+			if (availableBills.get(1).getBillAmount() <= 7 && !warning50MessageSent) {
+				
+				MockMail.send("Warning. Number of 50 bills is under 15%.");
+				warning50MessageSent = true;
+			}
+		}
 		return new ATMOutput(returnBills, message);
 	}
 	
