25528bcfd280543c995d08d47cf358216d6e8a44
diana <dianagiosan@yahoo.com>
2020-07-15 13:02:56 +0300

    Caught InputMismatchException in case the user inserts something other than a sum of cash.

--- src/ATMDriver.java
+++ src/ATMDriver.java
@@ -1,12 +1,19 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ATMDriver {
 	public static void main(String[] args) {
 		ATM thisATM = new ATM();
 		while (true) {
+			int cashToWithdraw = 0;
 			Scanner scanner = new Scanner(System.in);
-			int cashToWithdraw = scanner.nextInt();
-			System.out.println(thisATM.splitIntoBills(cashToWithdraw));
+			try {
+				cashToWithdraw = scanner.nextInt();
+				System.out.println(thisATM.splitIntoBills(cashToWithdraw));
+			} catch (InputMismatchException e) {
+				System.out.println("Insert a sum of cash to withdraw");
+			}
+			
 		}
 		
 	}
