51de2973d10746b277a28865d6f71cd58165858f
diana <dianagiosan@yahoo.com>
2020-07-15 10:55:26 +0300

    Added distinction between the exception thrown when the ATM has no cash left at all and when the transaction is not possible anymore as there are not enough of the necessary bills.

--- src/NotEnoughCashLeftException.java
+++ src/NotEnoughCashLeftException.java
@@ -1,6 +1,5 @@
-public class NotEnoughCashLeftException extends Exception { //the exception that is thrown when there is not enough
-	//cash left in the ATM for the transaction
-	public NotEnoughCashLeftException() {
-		super("There is no cash left in the ATM.");
+public class NotEnoughCashLeftException extends Exception {
+	public NotEnoughCashLeftException(){
+		super("The ATM ran out of cash. Please come back later.");
 	}
 }
